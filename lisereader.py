e5b09ff0f1097326dc2638543bd62c6bfe92637b
gwgwhc <georgehudsonchang@gmail.com>
2021-10-26 14:39:55 +0200

    changed get_info_all() output format

--- lisereader.py
+++ lisereader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 # =================== class =====================
 class LISEreader:
@@ -10,13 +11,16 @@ class LISEreader:
 
     def _read(self,filename):
         with open(filename) as f:
+            # for (i, line) in enumerate(f):
+            #     if "[Calculations]" in line:
+            #         file_start = i+1
+            #     # else:
+            #     #     raise #....
+
+            # lines=f.readlines()[file_start:]
             lines=f.readlines()[988:]
 
         self.data=[line.split()[0:6] + line.replace('=','').split()[7].split(',') for line in lines]
-
-    # def split
-    # def search
-    # def ...
 
     def get_info(self,name):
         l=[match for match in self.data if name in match][0]
@@ -24,7 +28,7 @@ class LISEreader:
 
     def get_info_all(self):
         data=np.array(self.data)
-        return [data[:,0], data[:,5], data[:,6]]
+        return np.transpose(np.array([data[:,0], data[:,5], data[:,6]]))
 
     def find_index(self,name):
         return [match for match in self.data if name in match][0]
@@ -36,14 +40,8 @@ filename="data/E143_TEline-ESR-72Ge.lpp"
 if __name__ == "__main__":
     try:
         lise_data=LISEreader(filename)
+        print(lise_data.get_info("77Br"))
         mydata=lise_data.get_info_all()
-        
+        print(mydata[3])
     except:
-        pass
-    
-    # except FileNotFoundError:
-        # print("File was not found, please enter a valid path.")
-    #     filename=input()
-    #     test_read(filename)
-    # else:
-    #     print("read ok.")
+        raise
