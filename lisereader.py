5510b3c5b626a3c93f7f444f101fde922e66d660
gwgwhc <georgehudsonchang@gmail.com>
2021-10-27 12:58:48 +0200

    added self.namedata, changed .get_index()

--- lisereader.py
+++ lisereader.py
@@ -1,5 +1,5 @@
 import numpy as np
-import re
+from re import sub 
 
 # =================== class =====================
 class LISEreader:
@@ -11,27 +11,36 @@ class LISEreader:
 
     def _read(self,filename):
         with open(filename) as f:
-            # for (i, line) in enumerate(f):
-            #     if "[Calculations]" in line:
-            #         file_start = i+1
-            #     # else:
-            #     #     raise #....
+            lines=f.readlines()
+    
+        for (i, line) in enumerate(lines):
+            if "[Calculations]" in line:
+                file_start = i+1
+            # else:
+            #     raise #....
 
-            # lines=f.readlines()[file_start:]
-            lines=f.readlines()[988:]
+        # self.data=[line.split()[0:6] + line.replace('=','').split()[7].split(',') 
+        # for line in lines[file_start:]]
 
-        self.data=[line.split()[0:6] + line.replace('=','').split()[7].split(',') for line in lines]
+        self.namedata = [line.split()[0] for line in lines[file_start:]]
+
+        self.data = [[sub("[^0-9]",'',line.split()[0])]
+        + line.replace("+","").split()[1:6] 
+        + line.replace('=','').split()[7].split(',') 
+        for line in lines[file_start:]]
+
+    def get_index(self,name):
+        return [i for i, element in enumerate(self.namedata) if name in element]
 
     def get_info(self,name):
         l=[match for match in self.data if name in match][0]
-        return l[0], l[5], l[6]
+        return [l[0], l[5], float(l[6])]
 
     def get_info_all(self):
         data=np.array(self.data)
         return np.transpose(np.array([data[:,0], data[:,5], data[:,6]]))
 
-    def find_index(self,name):
-        return [match for match in self.data if name in match][0]
+
 
 # ================== testing =====================
 
@@ -40,8 +49,13 @@ filename="data/E143_TEline-ESR-72Ge.lpp"
 if __name__ == "__main__":
     try:
         lise_data=LISEreader(filename)
-        print(lise_data.get_info("77Br"))
+        print(lise_data.get_info("77"))
         mydata=lise_data.get_info_all()
-        print(mydata[3])
+        print(mydata[-1])
+
+        print(lise_data.get_index("77Br"))
+
+        # print(lise_data.namedata)
+
     except:
         raise
