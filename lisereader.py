0ade58a105d9fa0fe79b6e1ff6f42684abc101c7
gwgwhc <georgehudsonchang@gmail.com>
2021-10-27 15:06:41 +0200

    changed get_info()

--- lisereader.py
+++ lisereader.py
@@ -24,23 +24,23 @@ class LISEreader:
 
         self.namedata = [line.split()[0] for line in lines[file_start:]]
 
-        self.data = [[sub("[^0-9]",'',line.split()[0])]
+        self.data = np.array(
+            [[sub("[^0-9]",'',line.split()[0])]
         + line.replace("+","").split()[1:6] 
         + line.replace('=','').split()[7].split(',') 
         for line in lines[file_start:]]
+        ,dtype=float)
 
     def get_index(self,name):
-        return [i for i, element in enumerate(self.namedata) if name in element]
+        return [i for i, element in enumerate(self.namedata) if name in element][0]
 
     def get_info(self,name):
-        l=[match for match in self.data if name in match][0]
-        return [l[0], l[5], float(l[6])]
+        index=self.get_index(name)
+        return [self.data[index][0],self.data[index][5],self.data[index][6]]
 
     def get_info_all(self):
         data=np.array(self.data)
         return np.transpose(np.array([data[:,0], data[:,5], data[:,6]]))
-
-
 
 # ================== testing =====================
 
@@ -49,13 +49,9 @@ filename="data/E143_TEline-ESR-72Ge.lpp"
 if __name__ == "__main__":
     try:
         lise_data=LISEreader(filename)
-        print(lise_data.get_info("77"))
-        mydata=lise_data.get_info_all()
-        print(mydata[-1])
-
+        print(lise_data.get_info("77Br"))
+        print(lise_data.get_info_all()[3])
         print(lise_data.get_index("77Br"))
-
-        # print(lise_data.namedata)
 
     except:
         raise
