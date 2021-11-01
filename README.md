bd0c49790f63823a2411a4e7309f1efa66083c98
gwgwhc <georgehudsonchang@gmail.com>
2021-11-01 12:09:23 +0100

    changed get_info_all() functionality

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 **#!! Construction in Progress !!#**
 
+Requires [Barion](https://github.com/xaratustrah/barion) from Xaratustrah to pull ame data.
+
 A test file can be found in `/data` to check that the program is operational. It will automatically be executed when running `lisereader.py`.
+
+# Functions
+
+For all search functions, the search string should be formatted "\[element]\[A]" e.g. "80Kr"
+
+`.get_index(name)` returns the line of the lise data that the searched element appears on
+
+`.get_info(name)` returns \[element name, A, N, Z, charge state, cross section] for the searched element
+
+`.get_info_all` returns a list of data for all the elements taken from the lise file, formatted identically to .get_info()
--- lisereader.py
+++ lisereader.py
@@ -2,10 +2,6 @@ import numpy as np
 import amedata
 from re import sub 
 
-# ame table:
-#       N  Z  A Element
-# index 3, 4, 5,   6
-
 # =================== class =====================
 class LISEreader:
     def __init__(self,filename):
@@ -29,48 +25,47 @@ class LISEreader:
             # else:
             #     raise #....
 
-        # self.data=[line.split()[0:6] + line.replace('=','').split()[7].split(',') 
-        # for line in lines[file_start:]]
-
-        self.namedata = [line.split()[0] for line in lines[file_start:]]
-
-        self.data = np.array(
-            [[sub("[^0-9]",'',line.split()[0])]
-        + line.replace("+","").split()[1:6] 
-        + line.replace('=','').split()[7].split(',') 
+        self.data=[line.replace('+','').split()[0:6] + line.replace('=','').split()[7].split(',') 
         for line in lines[file_start:]]
-        ,dtype=float)
 
     def get_index(self,name):
-        return [i for i, element in enumerate(self.namedata) if name in element][0]
+        return [i for i, element in enumerate(self.data) if name in element][0]
 
     def get_info(self,name):
         # retrieves charge state and cross section from lise data
-        index = self.get_index(name)
-        from_lise = [int(self.data[index][5]),self.data[index][6]]
+        index       = self.get_index(name)
+        from_lise   = [int(self.data[index][5]),float(self.data[index][6])]
 
         # retrieves name, A, Z, N from ame data
-        element, mass_number=sub("[0-9]",'',name), int(sub("[^0-9]",'',name))
-        from_ame = [[self.ame_data[i][6], self.ame_data[i][5], self.ame_data[i][4], self.ame_data[i][3]]
+        element     = sub("[0-9]",'',name)
+        mass_number = int(sub("[^0-9]",'',name))
+        from_ame    = [[self.ame_data[i][6], self.ame_data[i][5], self.ame_data[i][4], self.ame_data[i][3]]
         for i,line in enumerate(self.ame_data) if (element in line and mass_number==line[5])][0]
 
         if bool(from_lise)==True:
             return from_ame + from_lise
         else:
             return "check search format (e.g. \"80Kr\" )"
+            # or raise
 
     def get_lise_all(self):
         data=np.array(self.data)
-        return np.transpose(np.array([data[:,0], data[:,5], data[:,6]], dtype=float))
+        return np.transpose([data[:,0], data[:,5], data[:,6]])
+
+    def get_info_all(self):
+        return [self.get_info(line[0]) for line in self.data]
 
 # ================== testing =====================
-
 filename="data/E143_TEline-ESR-72Ge.lpp"
+lise_data=LISEreader(filename)
+
+def test1():
+    print(lise_data.get_lise_all()[0])
+    print(lise_data.get_info("80Kr"))
+    print(lise_data.get_info_all()[:5])
 
 if __name__ == "__main__":
     try:
-        lise_data=LISEreader(filename)
-        print(lise_data.get_info("25Mg"))
-        print(lise_data.get_lise_all()[:1])
+        test1()
     except:
         raise
