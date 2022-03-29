94932a7b5fdcb37a682fbaafe4cb6691dbb617f7
gwgwhc <georgehudsonchang@gmail.com>
2022-03-29 14:28:16 +0200

    updated parser descriptions

--- lisereader/__main__.py
+++ lisereader/__main__.py
@@ -6,16 +6,16 @@ from .version import __version__
 def main():
     scriptname = 'lisereader' 
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', type=str, default='test/E143_TEline-ESR-72Ge.lpp', help='Name of the LISE file to read.')
+    parser.add_argument('filename', type=str, default='test/E143_TEline-ESR-72Ge.lpp',
+                        help='Path of the LISE file you want to read.')
     parser.add_argument('-n', '--nuclei', type=str, nargs='?',
                         help='Nuclei name to get its info.')
     
     parser.add_argument('-i', '--ian',
-                        help='Get info for all the nuclei in the LISE file', action='store_true')
+                        help='Get information for all the nuclei in the LISE file', action='store_true')
     parser.add_argument('-v', '--verbose',
                         help='Increase output verbosity', action='store_true')
     
-
     args = parser.parse_args()
     
     print(f'Running {scriptname} V{__version__}')
